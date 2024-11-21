7d175a923af4d1b49a08fe2f11730779b07eabfe
Artag/Chris <scristxyz@gmail.com>
2024-11-21 11:08:04 -0500

    nast

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,18 +1,25 @@
 import 'dotenv/config';
-import * as joi from 'joi';
+import Joi, * as joi from 'joi';
 
 interface EnvConfig {
     PORT: number;
     DATABASE_URL: string;
+
+    NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi.object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items( joi.string() ).required(),
 })
 .unknown(true);
 
-const { error, value} = envVarsSchema.validate(process.env);
+const { error, value} = envVarsSchema.validate(
+    {...process.env,
+        NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+}
+);
 
 if (error) {
     throw new Error(`Config validation error: ${error.message}`);
@@ -22,5 +29,7 @@ const envVars: EnvConfig = value;
 
 export const envs={
 port: envVars.PORT,
-database_url: envVars.DATABASE_URL
+database_url: envVars.DATABASE_URL,
+nats_servers: envVars.NATS_SERVERS
+
 }
